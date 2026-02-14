57932abb8d2eca6070c741c34ac65f8542aa0dc3
Mashyren <federicomatthewpratamaa@gmail.com>
2026-02-14 18:51:19 +0700

    Fix: Backend panic on the regex check strength password

--- helper/helper.go
+++ helper/helper.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"math/rand"
 	"regexp"
+	"unicode"
 )
 
 func IsValidEmail(email string) bool {
@@ -11,8 +12,36 @@ func IsValidEmail(email string) bool {
 }
 
 func IsStrongPassword(password string) bool {
-	regex := `^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$`
-	return regexp.MustCompile(regex).MatchString(password)
+	if len(password) < 8 {
+		return false
+	}
+
+	var (
+		hasMinLen  = false
+		hasUpper   = false
+		hasLower   = false
+		hasNumber  = false
+		hasSpecial = false
+	)
+
+	if len(password) >= 8 {
+		hasMinLen = true
+	}
+
+	for _, char := range password {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
+		case unicode.IsNumber(char):
+			hasNumber = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+
+	return hasMinLen && hasUpper && hasLower && hasNumber && hasSpecial
 }
 
 func GenerateOTP() string {
